6d10efd9d406ffac8f302a30087ba675c61d7f1a
Steve <zhoudongyu08@gmail.com>
2019-03-14 21:49:30 +0800

    首页轮播图

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import homeHeader from './components/header'
+import homeSwiper from './components/swiper'
 export default {
   name: 'home',
   components: {
-    homeHeader
+    homeHeader,
+    homeSwiper
   }
 }
 </script>
