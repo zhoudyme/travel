c73aeaa367ebbc400319a149247faaf1c116b1dc
Steve <zhoudongyu08@gmail.com>
2019-03-17 21:55:27 +0800

    城市选择页面布局

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,18 +2,24 @@
   <div>
     <city-header></city-header>
     <city-Search></city-Search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
 import cityHeader from './components/header'
 import citySearch from './components/search'
+import cityList from './components/list'
+import cityAlphabet from './components/alphabet'
 
 export default {
   name: 'city',
   components: {
     cityHeader,
-    citySearch
+    citySearch,
+    cityList,
+    cityAlphabet
   }
 }
 </script>
